27acc98d1bc44d48d21b501afadb11f04a876c4a
= <gohelbhargav401@gmail.com>
2025-10-13 18:50:37 +0530

    Update Comment socket added

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -6,10 +6,8 @@ import Post from "./models/post.model.js";
 
 dotenv.config();
 
-// ✅ Create HTTP server (not app.listen)
 export const server = createServer(app);
 
-// ✅ Attach socket.io to the same HTTP server
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173", "http://localhost:5174"],
@@ -18,47 +16,81 @@ const io = new Server(server, {
 });
 
 // methods of the server
-async function updateLikes(postId,userId,socket){
-try {
-      const post = await Post.findById(postId);
-      if (!post) {
-        socket.emit("errorPostLike", { message: "Post Not Found" });
-        return;
-      }
-
-      if (!post.post_likes.includes(userId)) {
-        post.post_likes.push(userId);
-        await post.save();
-
-        io.emit("LikeUpdate", {
-          postId: post._id,
-          likeCount: post.post_likes.length,
-          message: "Post Liked",
-        });
-      } else {
-        post.post_likes = post.post_likes.filter(
-          (Id) => Id.toString() !== userId.toString()
-        );
-        await post.save();
-        io.emit("LikeUpdate", {
-          postId: post._id,
-          likeCount: post.post_likes.length,
-          message: "Post DisLiked",
-        });
-      }
-    } catch (error) {
-      console.error("Error in likePost socket event:", error);
-      socket.emit("errorPostLike", { message: "Internal Error in Post Like" });
+async function updateLikes(postId, userId, socket) {
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      socket.emit("errorPostLike", { message: "Post Not Found" });
+      return;
     }
+
+    if (!post.post_likes.includes(userId)) {
+      post.post_likes.push(userId);
+      await post.save();
+
+      io.emit("LikeUpdate", {
+        postId: post._id,
+        likeCount: post.post_likes.length,
+        message: "Post Liked",
+      });
+    } else {
+      post.post_likes = post.post_likes.filter(
+        (Id) => Id.toString() !== userId.toString()
+      );
+      await post.save();
+      io.emit("LikeUpdate", {
+        postId: post._id,
+        likeCount: post.post_likes.length,
+        message: "Post DisLiked",
+      });
+    }
+  } catch (error) {
+    console.error("Error in likePost socket event:", error);
+    socket.emit("errorPostLike", { message: "Internal Error in Post Like" });
+  }
 }
 
+async function updateComment(postId, comment, userId) {
+  try {
+    const newCommentOnPost = { user: userId, text: comment };
+    const post = await Post.findById(postId);
+    if (!post) {
+      socket.emit("errorPostLike", { message: "Post Not Found" });
+      return;
+    }
 
-// ✅ Socket.IO connection handler
+    const newComment = await Post.findByIdAndUpdate(
+      postId,
+      { $push: { post_comments: newCommentOnPost } },
+      { new: true }
+    )
+      .populate("post_comments.user", "name user_avatar")
+      .select("-_id -postdAt -updatedAt -createdAt -__v");
+    await newComment.save();
+
+    console.log(newComment.post_comments.length);
+
+    io.emit("CommentUpdate", {
+      postId: post._id,
+      Comment: newComment,
+      commentCount: newComment.post_comments.length,
+      message: "Comment Successfully",
+    });
+  } catch (error) {
+    console.error("Error in likePost socket event:", error);
+    socket.emit("errorPostLike", { message: "Internal Error in Post comment" });
+  }
+}
+
 io.on("connection", (socket) => {
   console.log("Socket connected:", socket.id);
 
   socket.on("likePost", async ({ postId, userId }) => {
-        updateLikes(postId,userId,socket);
+    updateLikes(postId, userId, socket);
+  });
+
+  socket.on("commentPost", async ({ postId, comment, userId }) => {
+        updateComment(postId,comment,userId)
   });
 
   socket.on("disconnect", () => {
