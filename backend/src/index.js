498353c9003eea92ebb36f39cccc52718413dc21
= <gohelbhargav401@gmail.com>
2025-10-18 14:11:25 +0530

    Connection change for socket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 import app from "./app.js";
 import { db } from "./db/dbConfig.js";
+import { server } from "./Server.js"
 
 const port = process.env.PORT || 5000;
 
 db()
   .then(() => {
-    app.listen(port, () => console.log(`Server is running on port ${port}`));
+    server.listen(port, () => console.log(`Server is running on port ${port}`));
   })
   .catch((err) => {
     console.error("failed to connect to database", err);
